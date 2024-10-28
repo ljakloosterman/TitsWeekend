4b1f92ab80e78a6b2bbe2826763eafd6f0451aae
Luc KLoosterman <luc.kloosterman@itility.nl>
2024-10-28 17:30:37 +0100

    V2.0

--- main.py
+++ main.py
@@ -1,27 +1,76 @@
 """Script to show payment progress"""
 import datetime
+import webbrowser
 import pandas as pd
 import streamlit as st
+
+
+def get_months():
+    """Get months until october 2025"""
+
+    # Get current date
+    today = datetime.datetime.now()
+
+    # Get months until october 2025
+    end = datetime.datetime(2025, 10, 1)
+    months = (end.year - today.year) * 12 + end.month - today.month
+
+    return months
+
+
+def add_transaction(transactions, tit, value):
+    """Adds transaction and updates json"""
+
+    # Add new transaction
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    new_transaction = pd.DataFrame({
+        "Datum": [today],
+        "Tit": [tit],
+        "Bedrag": [value]
+    })
+    transactions = pd.concat([transactions, new_transaction])
+
+    # Save transactions as json
+    transactions.to_json("transactions.json", orient='records', indent=4)
+    st.rerun()
 
 
 def main():
     """Main function of the script"""
 
-    # Load data
-    data = pd.read_csv("overview.csv", delimiter=",", index_col=0)
+    # Load transactions
+    transactions = pd.read_json("transactions.json")
 
     # Get remaining months untill october 2025
-    today = datetime.datetime.now()
-    end = datetime.datetime(2025, 10, 1)
-    months = (end.year - today.year) * 12 + end.month - today.month
+    months = get_months()
+
+    # Define tits
+    tits = [
+        "Stan",
+        "Roeland",
+        "Michiel",
+        "Joost",
+        "Mees",
+        "Yasper",
+        "Vincent",
+        "Bart",
+        "Gijs",
+        "Ernst",
+        "Sam",
+        "Nout",
+        "Max",
+        "Luc"
+        ]
+
+    # payment link
+    link = "https://www.ing.nl/payreq/m/?trxid=qHXrcxrMH5kKrcKxMOcPTjOPXWWaVAvT"
 
     # Set goals
     goal_pp = (13 - months) * 50
-    goal = 12*50*len(data.columns)
+    goal = 12*50*len(tits)
 
-    # Calculate total
-    total_pp = data.sum(axis=0)
-    total = total_pp.sum()
+    # Get total money saved
+    total = transactions["Bedrag"].sum()
 
     # Show title
     st.title("Titsweekend betalingsoverzicht 💸")
@@ -32,16 +81,52 @@ def main():
 
     # Show individual metrics
     st.markdown("### Individueel gepayerd")
-    for tit in data.columns:
-        delta = total_pp[tit] - goal_pp
-        st.metric(
-            label=tit,
-            value=f"€ {total_pp[tit]},-",
-            delta=float(delta))
+    for tit in tits:
+        col1, col2, col3 = st.columns(3)
 
-    # Show table
+        # Column for status
+        with col1:
+            total_pp = transactions.loc[
+                transactions["Tit"] == tit, "Bedrag"
+            ].sum()
+            delta = total_pp - goal_pp
+            st.metric(
+                label=tit,
+                value=f"€ {total_pp},-",
+                delta=float(delta))
+
+        # Column to add transactions
+        with col2:
+            value = st.number_input(
+                "Nieuwe betaling:",
+                key=f"value{tit}",
+                step=1.0,
+                min_value=50.0,
+                max_value=600.0
+                )
+
+        with col3:
+            if st.button("Betalen met je bek", key=f"button_{tit}"):
+
+                # Open new tab
+                webbrowser.open(link, new=2)
+
+                # Add transaction
+                add_transaction(transactions, tit, value) 
+
+        # Divider for clarity
+        st.divider()
+
+    # Show transactions
     st.markdown("### Transacties")
-    st.dataframe(data)
+    st.dataframe(
+        transactions,
+        use_container_width=True,
+        hide_index=True,
+        column_config={
+            "Bedrag": st.column_config.NumberColumn(format="€%d")
+        },
+    )
 
 if __name__ == "__main__":
     main()
